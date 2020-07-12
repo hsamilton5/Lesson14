cea493856f068f127a52f1a7ed1b8420f93da15c
hsamilton5 <38704464+hsamilton5@users.noreply.github.com>
2020-07-12 18:07:21 -0400

    Add files via upload

---
+++ build/precache-manifest.2038c9e7fe65626adeb1f8a7ed84ad83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008d3a86abef9ac2df251d07c19663b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "515efcbc9f1f60e527ad",
+    "url": "./static/css/main.af5d8795.chunk.css"
+  },
+  {
+    "revision": "d774baae181057cf21e2",
+    "url": "./static/js/2.162c85fc.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "./static/js/2.162c85fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "515efcbc9f1f60e527ad",
+    "url": "./static/js/main.a83ebe7b.chunk.js"
+  },
+  {
+    "revision": "0e8608933f310658b8d9",
+    "url": "./static/js/runtime-main.d107943c.js"
+  },
+  {
+    "revision": "89b517fa0cf889b48c4b325eebb7f69c",
+    "url": "./static/media/sam_photo.89b517fa.jpg"
+  }
+]);
